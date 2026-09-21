1a102769846181f53a3a20f847614eb4ea724c09
Thomas Jöckel <thomas@joeckel.io>
2026-09-21 23:19:16 +0200

    exp: add dedicated MoE ID copy stream state

--- ggml/src/ggml-cuda/common.cuh
+++ ggml/src/ggml-cuda/common.cuh
@@ -1471,6 +1471,9 @@ struct ggml_backend_cuda_context {
     ggml_cuda_moe_grouped_context * moe_grouped_context = nullptr;
     std::vector<std::unique_ptr<ggml_backend_cuda_context>> moe_router_contexts;
     std::unique_ptr<ggml_cuda_moe_ids_cache_state> moe_ids_cache;
+    cudaStream_t moe_ids_copy_stream = nullptr;
+    cudaEvent_t moe_ids_ready_event = nullptr;
+    cudaEvent_t moe_ids_done_event = nullptr;
 
     cudaStream_t streams[GGML_CUDA_MAX_DEVICES][GGML_CUDA_MAX_STREAMS] = { { nullptr } };
     cublasHandle_t cublas_handles[GGML_CUDA_MAX_DEVICES][GGML_CUDA_MAX_STREAMS] = {nullptr};
