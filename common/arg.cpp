c63f6a35a9823ff4540648c33fb7a2886fe897cc
Thomas Jöckel <thomas@joeckel.io>
2026-09-22 18:09:35 +0200

    exp: allow CUDA_Host tensor overrides

--- common/arg.cpp
+++ common/arg.cpp
@@ -259,6 +259,10 @@ static void parse_tensor_buffer_overrides(const std::string & value, std::vector
         if (buft) {
             buft_list[ggml_backend_buft_name(buft)] = buft;
         }
+        auto * host_buft = ggml_backend_dev_host_buffer_type(dev);
+        if (host_buft) {
+            buft_list[ggml_backend_buft_name(host_buft)] = host_buft;
+        }
     }
 
     for (const auto & override : string_split<std::string>(value, ',')) {
